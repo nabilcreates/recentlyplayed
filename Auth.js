222c07e7b42b44390410a0220f4ca70b3ccf4cf1
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2019-04-08 03:50:03 +0800

    Some of the request parameters are in variables so its easier to understand or change or maintain

--- Auth.js
+++ Auth.js
@@ -1,5 +1,8 @@
+let redirect_uri = 'https://renabil.github.io/recentlyplayed/Auth.html'
+let scopes = 'user-read-currently-playing'
+
 let url = document.URL
-let spotify_auth_link = 'https://accounts.spotify.com/authorize?client_id=3a89b22b7095445782078c237454dafd&redirect_uri=http://127.0.0.1:8080/Auth.html&scope=user-read-currently-playing&response_type=token&state=123'
+let spotify_auth_link = `https://accounts.spotify.com/authorize?client_id=3a89b22b7095445782078c237454dafd&redirect_uri=${redirect_uri}&scope=${scopes}&response_type=token`
 
 if(url.includes('access_token')){
     let access_token = url.split('#')[1].split('=')[1].split('&')[0]
