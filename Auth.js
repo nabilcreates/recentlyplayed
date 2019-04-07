a5a07015d84019af1b01a1f800d51ca9d1ab2c86
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2019-04-08 03:54:59 +0800

    Straight redirect using Spotify API to Home Page

--- Auth.js
+++ Auth.js
@@ -1,13 +1,7 @@
-let redirect_uri = 'https://renabil.github.io/recentlyplayed/Auth.html'
+let redirect_uri = 'https://renabil.github.io/recentlyplayed/'
 let scopes = 'user-read-currently-playing'
 
 let url = document.URL
 let spotify_auth_link = `https://accounts.spotify.com/authorize?client_id=3a89b22b7095445782078c237454dafd&redirect_uri=${redirect_uri}&scope=${scopes}&response_type=token`
 
-if(url.includes('access_token')){
-    let access_token = url.split('#')[1].split('=')[1].split('&')[0]
-    console.log(url)
-    window.location.replace('/?access_token=' + access_token)
-}else{
-    window.location.replace(spotify_auth_link)
-}
+window.location.replace(spotify_auth_link)
